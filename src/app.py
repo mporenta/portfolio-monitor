ceeaf8d9e03ac280ee76b5b097784bc06de5c9a8
Jon Joe Joe <mike.porenta@gmail.com>
2024-11-09 20:15:41 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -77,7 +77,7 @@ def get_trades():
         return jsonify({'status': 'error', 'message': 'Failed to fetch trades data'}), 500
 
 def run_flask_app():
-    app.run(host='127.0.0.1', port=5001, use_reloader=False)
+    app.run(host='0.0.0.0', port=5001, use_reloader=False)
 
 if __name__ == '__main__':
     try:
