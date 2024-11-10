5394878809aaaf1fd401ed35be958ae96f151fa6
Jon Joe Joe <mike.porenta@gmail.com>
2024-11-09 21:48:43 -0700

    Update run-python.sh

--- run-python.sh
+++ run-python.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+sleep 30
+# Change directory to the specified path
+cd /home/tbot/develop/github/portfolio-monitor/src || exit
+
+# Run main.py using Python (ensure Python is installed and configured)
+python3 pnl_monitor.py
 sleep 10
-python3 ./src/main.py
+python3 app.py
