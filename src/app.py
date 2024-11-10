c91c73ff41a0618748fdf8ad1a625d04e2219476
Jon Joe Joe <mike.porenta@gmail.com>
2024-11-10 07:12:12 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -79,13 +79,12 @@ def get_trades():
         logger.error(f"Error fetching trades: {str(e)}")
         return jsonify({'status': 'error', 'message': 'Failed to fetch trades data'}), 500
 
-def run_flask_app():
-    app.run(host='/', port=5001, use_reloader=False)
 
-if __name__ == '__main__':
-    try:
-        logger.info("Starting Flask app.")
-        run_flask_app()
-    
-    except Exception as e:
-        logger.error(f"Error starting Flask app: {str(e)}")
+if __name__ == "__main__":
+    port = int(os.getenv("PNL_HTTPS_PORT", "5001"))
+    if strtobool(os.getenv("TBOT_PRODUCTION", "False")):
+        serve(app, host="0.0.0.0", port=port)
+    else:
+        app.run(debug=True, host="0.0.0.0", port=port)
+
+
