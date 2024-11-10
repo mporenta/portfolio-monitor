1e2284b1a2ddf18d4260eacfa1fd87c869542398
Jon Joe Joe <mike.porenta@gmail.com>
2024-11-10 12:24:58 -0700

    Update run-python.sh

--- run-python.sh
+++ run-python.sh
@@ -3,8 +3,9 @@
 # Navigate to the directory containing the Python files
 cd /home/tbot/develop/github/portfolio-monitor/src
 
+# Start the Flask app
+python3 app.py
+
 # Run the PnL monitor script
 python3 pnl_monitor.py
 
-# Start the Flask app
-flask --app app run
