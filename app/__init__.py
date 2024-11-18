36458f4847080f6985d8d3aa197de5781e2b71de
Jon Joe Joe <mike.porenta@gmail.com>
2024-11-18 10:41:37 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,2 @@
 # -*- coding: utf-8 -*-
 """__init__.py"""
-from fastapi import FastAPI, HTTPException, Request, Response, Depends
-from fastapi.responses import JSONResponse, HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel, Field
-from typing import List, Dict, Optional, Union, Any
-from sqlalchemy.orm import Session
-# Import SQLAlchemy models with different names to avoid conflicts
-from db import (
-    SessionLocal, 
-    Position as DBPosition,  # Rename the SQLAlchemy model import
-    PnLData,
-    Trade,
-    Order
-)
-
-
